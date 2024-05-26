0bfb241e455f9092d9e45ecadfe41205fe3976c5
jimmy.lopezmorales <jimmy.lopezmorales@utb.ecutbildning.se>
2024-05-26 14:08:00 +0200

    Start of Project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-xl font-bold underline text-red-700">
+      Hello world!
+    </h1>
+    <h2 className="text-red-500">Why is it now working</h2>
+    </>
+  )
+}
+
+export default App
